9139dce42012fc47309a744c002639f4c9429c9c
Robz187 <saliger.robert@gmail.com>
2023-11-27 14:35:14 +0100

    changed 2 CoinCap Api for history requests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
-const apiURL = "https://api.blockchain.com/v3/exchange";
+const apiURL = 'https://api.coincap.io/v2';
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +13,10 @@ app.get("/",(req,res)=>{
 
     res.render("index.ejs");
 });
+app.get("/currency",(req,res)=>{
 
+    res.redirect("/");
+})
 app.post("/",(req,res)=>{
 
     res.render("index.ejs");
@@ -21,11 +24,14 @@ app.post("/",(req,res)=>{
 });
 
 app.post("/currency",async(req,res)=>{
-    const currency = req.body.currency;
-    try
+ try
     {   
-        const result = await axios.get(apiURL+"/tickers/"+currency);
-        res.render("currency.ejs",{data:result.data});
+        const currency = req.body.currency;
+        const result = await axios.get(apiURL+"/assets/"+currency);
+        const jsonData = JSON.stringify(result.data);
+        const data = JSON.parse(jsonData);
+        console.log (data);
+        res.render("currency.ejs",data);
     }catch (error)
     {
         console.error(error.message);
